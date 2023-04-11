f26af0becc6dfe612609b82205f80498250d6312
Jose Antonio Toro Ospina <94059096+josetoro9@users.noreply.github.com>
2023-04-11 10:20:23 -0500

    Update sql_globant.sql

--- sql_globant.sql
+++ sql_globant.sql
@@ -37,9 +37,69 @@ from
 where
     year(cast(LEFT(datetime, 10) as date)) = 2021;
 
-select
-    a.*
-from
-    mydatabase.hired_employees_mod a
-    left join;
-
+with adaptation as (
+    select
+        id,
+        MONTH(datetime) as month,
+        IFNULL(department_id, 0) as department_id,
+        IFNULL(job_id, 0) as job_id
+    from
+        mydatabase.hired_employees_mod
+),
+distincts as (
+    select
+        department_id,
+        job_id
+    from
+        adaptation
+    group by
+        1,
+        2
+),
+conteos as (
+    select
+        department_id,
+        job_id,
+        month,
+        count(*) as conteo
+    from
+        adaptation
+    group by
+        1,
+        2,
+        3
+),
+quarters as (
+    select
+        department_id,
+        job_id,
+        case
+            when month between 1
+            and 3 then 1
+            when month between 4
+            and 6 then 2
+            when month between 7
+            and 9 then 3
+            when month between 10
+            and 12 then 4
+        end as qtr,
+        conteo
+    from
+        conteos
+),
+sums as (
+    select
+        department_id,
+        job_id,
+        qtr,
+        sum(conteo) as total
+    from
+        quarters
+    group by
+        1,
+        2,
+        3
+)
+select a.*,IFNULL(b.total, 0) as q1 from distincts a 
+left join (select * from sums where qtr=1) b on a.department_id=b.department_id and a.job_id=b.job_id
+;
