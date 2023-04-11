97571cef81c1846658e1a622d34f39aad87f3ee4
Jose Antonio Toro Ospina <94059096+josetoro9@users.noreply.github.com>
2023-04-11 15:10:11 -0500

    Reorganize

---
+++ data_preparation.sql
@@ -0,0 +1,38 @@
+ALTER TABLE
+    mydatabase.hired_employees
+MODIFY
+    id int;
+
+ALTER TABLE
+    mydatabase.hired_employees
+MODIFY
+    department_id int;
+
+ALTER TABLE
+    mydatabase.hired_employees
+MODIFY
+    job_id int;
+
+-------------------------------
+ALTER TABLE
+    mydatabase.jobs
+MODIFY
+    id int;
+
+ALTER TABLE
+    mydatabase.departments
+MODIFY
+    id int;
+
+-- whe have to do this table beacause of the main preferences of safe mode
+create table mydatabase.hired_employees_mod as
+select
+    id,
+    name,
+    cast(LEFT(datetime, 10) as date) as datetime,
+    department_id,
+    job_id
+from
+    mydatabase.hired_employees
+where
+    year(cast(LEFT(datetime, 10) as date)) = 2021;
